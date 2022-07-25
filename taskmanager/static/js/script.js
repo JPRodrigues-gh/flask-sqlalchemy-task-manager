4dec6f33a5380a7afaf6797c9f74cd73788c781f
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-07-25 08:52:30 +0000

    Add task route and function to add task form

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // sidenav Initialization
     var sidenav = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(sidenav);
+    M.Sidenav.init(sidenav);
+
+    // Date Picker Initialization
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // select initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
