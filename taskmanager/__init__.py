5beac921fbac80a500278d72d805d5778a86b19c
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-07-26 12:42:30 +0000

    Add condition to check whether app is on DEVELOPMENT or not to set correct DB env -- __init__

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,7 +10,11 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECURITY_KEY"] = os.environ.get("SECURITY_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 db = SQLAlchemy(app)
 
